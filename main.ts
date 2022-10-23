1d1f7d29ad05a0069ef9b768d1c3e1fe23903731
psocc <116518712+psocc@users.noreply.github.com>
2022-10-23 15:38:45 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        # # # . .
+        # . . . .
+        # . . . .
+        `)
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M1,
+    70,
+    SuperBit.enMotors.M1,
+    70
+    )
+})
+basic.showIcon(IconNames.Square)
+basic.forever(function () {
+	
+})
